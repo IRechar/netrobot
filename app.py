4efdfaa5be835f8df37e841740f67ee3732f9f48
bminixhofer <bminixhofer@gmail.com>
2017-04-21 15:07:26 +0200

    fixed app.py for rpi

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, current_app, request, render_template, Response
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import subprocess
 import time
 
-# GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
 motors = {
   'front_left': (4, 17, 18)
@@ -13,9 +13,9 @@ motors = {
 }
 
 # setup
-# for motor in motors:
-# 	for port in motors[motor]:
-# 		GPIO.setup(port, GPIO.OUT)
+for motor in motors:
+	for port in motors[motor]:
+		GPIO.setup(port, GPIO.OUT)
 
 app = Flask(__name__)
 subprocess.call(['sudo', 'bash', './stream.sh'])
@@ -24,7 +24,7 @@ print("IP: " + str(host_ip))
 
 @app.route('/')
 def index():
-	return render_template('index.html', stream_ip='http://192.168.43.152:8081/')
+	return render_template('index.html', stream_ip="http://" + host_ip + ":5000")
 
 @app.route('/move')
 def move():
@@ -41,15 +41,13 @@ def move():
 	  speed_b = speed;
 	  speed_a = speed * ((100 - abs(rotation)) / 100)
 	
-	return "A: {0}, B: {1}".format(speed_a, speed_b)
-# 	GPIO.output(4, GPIO.HIGH)
-# 	GPIO.output(17, GPIO.LOW)
-# 	GPIO.output(18, GPIO.HIGH)
+	GPIO.output(4, GPIO.HIGH)
+	GPIO.output(17, GPIO.LOW)
+	GPIO.output(18, GPIO.HIGH)
 
 @app.route('/stop')
 def stop():
-	pass
-	# GPIO.output(18, GPIO.LOW)
+	GPIO.output(18, GPIO.LOW)
 
 if(__name__ == '__main__'):
 	app.run(debug=True, host='0.0.0.0')
